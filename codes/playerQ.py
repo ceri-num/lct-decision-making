64451488ebea7c4184720845d7f2f2b3523184f6
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2021-04-20 12:04:04 +0200

    clean player Q

--- codes/playerQ.py
+++ codes/playerQ.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Default game interface :
 def main():
-    player= PlayerQ( explorationRatio=0.1, learningRate= 0.1 )
+    player= AgentQ( explorationRatio=0.1, learningRate= 0.1 )
     stats= { "exploration": [], "average score": [], "average best Q": [] }
     samples= 500
     step= 500
@@ -15,12 +15,12 @@ def main():
             gameEngine.run( player )
             total+= player.score
         # Record exploration indicator: the number of visited states 
-        sizeQ= len(player.Q)
+        sizeQ= len(player.qvalues)
         stats["exploration"].append( sizeQ )
         # Record the average score:
         sumQ= 0
-        for s in player.Q :
-            sumQ+= player.Q[s][ player.actionMax(s) ]
+        for s in player.qvalues :
+            sumQ+= player.qvalues[s][ player.bestAction(s) ]
         stats["average score"].append( total/samples )
         # Record the average best Q value for each state:
         stats["average best Q"].append( sumQ/sizeQ )
@@ -31,56 +31,62 @@ def main():
         plt.show()
 
 # Agent as a very simple UI
-class PlayerQ :
+class AgentQ :
 
+    # Initialization:
     def __init__(self, explorationRatio=0.1, discountFactor=0.99, learningRate= 0.1 ):
         self.epsilon= explorationRatio
         self.gamma= discountFactor
         self.alpha= learningRate
-        self.Q= {
+        self.qvalues= {}
 
+    # Q learning methods:
+    def updateQ( self, aStateT0, anAction, aStateT1, aReward ) : 
+        oldValue= self.qvalues[aStateT0][anAction]
+        futureGains= self.qvalues[aStateT1][ self.bestAction(aStateT1) ]
+        self.qvalues[aStateT0][anAction]= (1 - self.alpha) * oldValue + self.alpha * ( aReward + self.gamma * futureGains )
+
+    def bestAction( self, aState ) : 
+        option= random.choice( self.actions )
+        for a in self.qvalues[aState] :
+            if self.qvalues[aState][a] > self.qvalues[aState][option] :
+                option= a 
+        return option
+
+    def printQ(self) :
+        print( "Q: " )
+        for s in self.qvalues :
+            print( s + ": " + str( self.qvalues[s] ) )
+
+    # Agent interfase:
     def wakeUp( self, initialState, stateDsc, actionSpace ):
         # Reccord initial state:
-        self.state= initialStateStr
+        self.state= initialState
         # Reccord the list of all possible actions:
         self.actions= actionSpace
         # Initialize a new state in Q if necessary:
-        if self.state not in self.Q.keys() :
-            self.Q[self.state]= { a: 0.0 for a in self.actions }
+        if self.state not in self.qvalues.keys() :
+            self.qvalues[self.state]= { a: 0.0 for a in self.actions }
 
-    def perceive(self, reachedStateStr, reward ):
+    def perceive(self, reachedState, reward ):
         # Initialize a new state in Q if necessary:
-        if reachedStateStr not in self.Q.keys() :
-            self.Q[reachedStateStr]= { a: 0.0 for a in self.actions }
-        # update Q( self.state, self.actionStr ) with reachedStateStr, reward
-        oldValue= self.Q[self.state][self.actionStr]
-        futureGains= self.Q[ reachedStateStr ][ self.actionMax(reachedStateStr) ]
-        self.Q[self.stateStr][self.actionStr]= (1 - self.alpha) * oldValue + self.alpha * ( reward + self.gamma * futureGains )
+        if reachedState not in self.qvalues.keys() :
+            self.qvalues[reachedState]= { a: 0.0 for a in self.actions }
+        # update Q( self.state, self.action ) with reachedState, reward
+        self.updateQ( self.state, self.action, reachedState, reward )
         # Switch the new state:
-        self.state= reachedStateStr
-
-    def actionMax( self, aState ) : 
-        option= random.choice( self.actions )
-        for a in self.Q[aState] :
-            if self.Q[aState][a] > self.Q[aState][option] :
-                option= a 
-        return option
+        self.state= reachedState
 
     def decide(self, isValidAction) : # pure exploration: 
         if random.random() < self.epsilon :
             self.action= random.choice( self.actions )
         else :
-            self.action= self.actionMax( self.stateStr )
+            self.action= self.bestAction( self.state )
         return self.action
 
     def sleep(self, score ) :
         # print( "Game end on score: "+ str(score) )
         self.score= score
-
-    def printQ(self) :
-        print( "Q: " )
-        for s in self.Q :
-            print( s + ": " + str( self.Q[s] ) )
 
 # Activate default interface :
 if __name__ == '__main__':
